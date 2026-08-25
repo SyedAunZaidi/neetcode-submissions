7555a7843abae9b80f4952ed38e37f6723d0d87c
Aun Zaidi <49536143+SyedAunZaidi@users.noreply.github.com>
2026-08-25 16:12:20 -0400

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        # bubble sort
+
+        length = len(nums)
+        for k in range(1, length):
+            for i in range(1, length):
+
+                if nums[i] < nums[i-1]:
+                    #swap
+                    nums[i], nums[i-1] = nums[i-1], nums[i]
+            
+        #     print(nums)
+
+
+        # print(nums)
